60d319461ccc7b478525c7f180a331b7b57b5530
Tom Hulihan <hulihan.tom159@gmail.com>
2013-06-26 12:24:25 -0400

    Refactored the Docker::Connection class.
    This will allow for far more concise requests throughout the rest of the
    gem.

--- lib/docker/connection.rb
+++ lib/docker/connection.rb
@@ -5,69 +5,62 @@ class Docker::Connection
 
   attr_reader :url, :options
 
-  # Create a new Connection. By default, the Connection points to localhost at
-  # port 4243, but this can be changed via an options Hash.
-  def initialize(url = 'http://localhost', options = {})
-    unless options.is_a?(Hash)
-      raise Docker::Error::ArgumentError, "Expected a Hash, got: #{options}"
+  # Create a new Connection. This method takes a url (String) and options
+  # (Hash). These are passed to Excon, so any options valid for `Excon.new`
+  # can be passed here.
+  def initialize(url, opts)
+    case
+    when !url.is_a?(String)
+      raise ArgumentError, "Expected a String, got: '#{url}'"
+    when !opts.is_a?(Hash)
+      raise ArgumentError, "Expected a Hash, got: '#{opts}'"
+    else
+      @url, @options = url, opts
     end
-    self.url = url
-    self.options = { :port => 4243 }.merge(options)
   end
 
-  # The actual client that sends HTTP methods to the docker server.
+  # The actual client that sends HTTP methods to the Docker server. This value
+  # is not cached, since doing so may cause socket errors after bad requests.
   def resource
-    @resource ||= Excon.new(self.url, self.options)
+    Excon.new(url, options)
+  end
+  private :resource
+
+  # Send a request to the server with the `
+  def request(*args, &block)
+    resource.request(compile_request_params(*args, &block)).body
+  rescue Excon::Errors::BadRequest => ex
+    raise ClientError, ex.message
+  rescue Excon::Errors::InternalServerError => ex
+    raise ServerError, ex.message
   end
 
-  # Nil out the connection. This now happens on every request to prevent socket
-  # errors.
-  def reset!
-    @resource = nil
-  end
-
-  # Delegate all HTTP methods to the resource.
-  [:get, :put, :post, :delete, :request].each do |method|
-    define_method(method) do |*args, &block|
-      begin
-        self.reset!
-        self.resource.public_send(method, *args, &block)
-      rescue Excon::Errors::BadRequest => ex
-        raise ClientError, ex.message
-      rescue Excon::Errors::InternalServerError => ex
-        raise ServerError, ex.message
-      end
-    end
-  end
-
-  # Send a request to the server and then parse it into a Hash.
-  def json_request(method, path, query = {}, &block)
-    params = compile_request_params(method, path, query, &block)
-    body = self.request(params).body
-    JSON.parse(body) unless body.nil? || body.empty? || (body == 'null')
-  rescue JSON::ParserError => ex
-    raise UnexpectedResponseError, ex.message
+  # Delegate all HTTP methods to the #request.
+  [:get, :put, :post, :delete].each do |method|
+    define_method(method) { |*args, &block| request(method, *args, &block) }
   end
 
   def to_s
-    "Docker::Connection { :url => #{self.url}, :options => #{self.options} }"
+    "Docker::Connection { :url => #{url}, :options => #{options} }"
   end
 
 private
-  attr_writer :url, :options
-
-  # Given an http_method, path, query, and optional block, returns the
-  # corresponding request parameters.
-  def compile_request_params(http_method, path, query, &block)
+  # Given an HTTP method, path, optional query, extra options, and block,
+  # compiles a request.
+  def compile_request_params(http_method, path, query = nil, opts = nil, &block)
+    query ||= {}
+    opts ||= {}
+    headers = opts.delete(:headers) || {}
     {
-      :method         => http_method,
-      :path           => path,
-      :query          => query,
-      :headers => { 'Content-Type' => 'text/plain',
-                    'User-Agent' => "Docker-Client/0.4.6" },
-      :expects        => (200..204),
-      :idempotent     => http_method == :get,
-      :response_block => block
-    }.reject { |_, v| v.nil? }
+      :method        => http_method,
+      :path          => "/v#{Docker::API_VERSION}#{path}",
+      :query         => query,
+      :headers       => { 'Content-Type' => 'text/plain',
+                          'User-Agent'   => 'Docker-Client/0.4.6'
+                        }.merge(headers),
+      :expects       => (200..204),
+      :idempotent    => http_method == :get,
+      :request_block => block
+    }.merge(opts).reject { |_, v| v.nil? }
   end
 end
--- lib/docker/version.rb
+++ lib/docker/version.rb
@@ -1,3 +1,7 @@
 module Docker
+  # The version of the docker-api gem.
   VERSION = '1.0.1'
+
+  # The version of the compatible Docker remote API.
+  API_VERSION = '1.3'
 end
