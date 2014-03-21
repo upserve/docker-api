ec4ae1cb023aff5ed4bf66da85226c07eb59767e
Tom Hulihan <hulihan.tom159@gmail.com>
2014-03-21 16:13:50 -0400

    Mock open-uri

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,9 @@
 require 'vcr'
+require 'webmock'
 
 VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = false
-  c.hook_into :excon
+  c.hook_into :excon, :webmock
   c.cassette_library_dir = File.join(File.dirname(__FILE__), '..', 'vcr')
   c.configure_rspec_metadata!
 end
