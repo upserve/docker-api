6a7a69bc8e38e0ae24ecccce068fa2139d9ec198
Todd Lunter <tlunter@gmail.com>
2014-08-20 23:31:17 -0400

    Clean up hijack

--- lib/excon/middlewares/hijack.rb
+++ lib/excon/middlewares/hijack.rb
@@ -4,6 +4,25 @@ module Excon
     # yields the underlying TCP socket for raw TCP communication (used to
     # attach to STDIN of containers).
     class Hijack < Base
+      def build_response(status, socket)
+        response = {
+          :body          => '',
+          :headers       => Excon::Headers.new,
+          :status        => status,
+          :remote_ip     => socket.respond_to?(:remote_ip) &&
+                            socket.remote_ip,
+        }
+        if socket.data[:scheme] =~ /^(https?|tcp)$/
+          response.merge({
+            :local_port    => socket.respond_to?(:local_port) &&
+                              socket.local_port,
+            :local_address => socket.respond_to?(:local_address) &&
+                              socket.local_address
+          })
+        end
+        response
+      end
+
       def response_call(datum)
         if datum[:hijack_block]
           # Need to process the response headers here rather than in
@@ -15,21 +34,7 @@ module Excon
           until match = /^HTTP\/\d+\.\d+\s(\d{3})\s/.match(socket.readline); end
           status = match[1].to_i
 
-          datum[:response] = {
-            :body          => '',
-            :headers       => Excon::Headers.new,
-            :status        => status,
-            :remote_ip     => socket.respond_to?(:remote_ip) &&
-                              socket.remote_ip,
-          }
-          if socket.data[:scheme] =~ /^(https?|tcp)$/
-            datum[:response].merge({
-              :local_port    => socket.respond_to?(:local_port) &&
-                                socket.local_port,
-              :local_address => socket.respond_to?(:local_address) &&
-                                socket.local_address
-          })
-          end
+          datum[:response] = build_response(status, socket)
 
           Excon::Response.parse_headers(socket, datum)
           datum[:hijack_block].call socket.instance_variable_get(:@socket)
