6b11b42175cc48a35fe1b364a3c581dfcd864c7e
Tom Duffield <tom@opscode.com>
2014-12-01 11:03:08 -0600

    Use DOCKER_HOST stub for VCR cassettes
    To address the issue where Travis was failing due to the URI not matching. The correct way to fix this issue is to use `:match_requests_on` but the issue is that the `URI` library can not accurately parse unix socket URIs. To get around this we are using the `filter_sensitive_data` feature to replace the host portion of the URI with a stub to be filled at runtime.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,10 @@
 require 'vcr'
 require 'webmock'
+require 'docker'
 
 VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = false
+  c.filter_sensitive_data('<DOCKER_HOST>') { Docker.url.sub(/tcp\:/, 'https:') }
   c.filter_sensitive_data('<USERNAME>') { ENV['DOCKER_API_USER'] }
   c.filter_sensitive_data('<PASSWORD>') { ENV['DOCKER_API_PASS'] }
   c.filter_sensitive_data('<EMAIL>') { ENV['DOCKER_API_EMAIL'] }
