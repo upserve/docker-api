d029362904fa755dedb241deb6a50cffad1ebf0c
Sergey Bahchissaraitsev <sergey@bahchis.com>
2015-05-10 01:39:24 +0300

    container options pretify capitalized support

--- lib/docker/container.rb
+++ lib/docker/container.rb
@@ -221,8 +221,7 @@ class Docker::Container
 
   # Create a new Container.
   def self.create(opts = {}, conn = Docker.connection)
-    name = opts.delete('Name')
-    name = opts.delete('name') if !name
+    name = opts.delete('Name') || opts.delete('name')
     query = {}
     query['name'] = name if name
     resp = conn.post('/containers/create', query, :body => opts.to_json)
