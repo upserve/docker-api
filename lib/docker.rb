cff5b1f6a62d405deb19fdda722a88936395a9fc
Cristiano Balducci <cristiano.balducci@gmail.com>
2015-11-24 13:21:14 +0100

    Added support for docker volume API (for Docker 1.9)  Changes to be committed: 	modified:   lib/docker.rb 	modified:   lib/docker/base.rb 	new file:   lib/docker/volume.rb 	new file:   spec/docker/volume_spec.rb

--- lib/docker.rb
+++ lib/docker.rb
@@ -30,6 +30,7 @@ module Docker
   require 'docker/messages'
   require 'docker/util'
   require 'docker/version'
+  require 'docker/volume'
   require 'docker/rake_task' if defined?(Rake::Task)
 
   def default_socket_url
