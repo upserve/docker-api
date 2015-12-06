b43f9835ba6ea418d75babb3de5c3220f94e3b99
Todd Lunter <tlunter@gmail.com>
2015-12-06 16:42:34 -0500

    Make sure docker is running

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -8,4 +8,16 @@ gem install bundler
 sudo mkdir -p /opt/docker
 sudo curl -fo /opt/docker/docker "https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}"
 sudo chmod +x /opt/docker/docker
-sudo /opt/docker/docker -d -D &
+
+running=0
+for x in {1..3}
+do
+    [[ $running != 1 ]] || break
+    sudo rm -rf /var/run/docker.pid
+    sudo /opt/docker/docker -d -D &
+    DOCKER_PID=$!
+    sleep 5
+    echo "Checking if docker is running"
+    ps -p $DOCKER_PID && running=1 || echo "Couldn't start docker, retrying"
+done
+echo "Docker running, continuing"
