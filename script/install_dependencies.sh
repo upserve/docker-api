45851c3af1774779f7570ed6d7894a37b0b280a2
Sean OMeara <sean@chef.io>
2016-02-22 18:14:10 -0500

    tweaking travis

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -6,7 +6,8 @@ set -e
 echo 'deb "https://apt.dockerproject.org/repo" wily-backports main' >> /etc/apt/sources.list.d/wily-backports.list
 
 # enable docker repo
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+# apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb "https://apt.dockerproject.org/repo" ubuntu-wily main' >> /etc/apt/sources.list.d/docker-main.list
 
 # update apt cache
