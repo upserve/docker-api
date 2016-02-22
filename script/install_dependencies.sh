73e741230308d7cb07ebb99b79da43006ea0d23a
Sean OMeara <sean@chef.io>
2016-02-22 17:37:50 -0500

    tweaking travis

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -2,9 +2,6 @@
 set -x
 set -e
 
-# Update bundler
-gem install bundler
-
 # Install docker
 # sudo mkdir -p /opt/docker
 # sudo curl -fo /opt/docker/docker "https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}"
