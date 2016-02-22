f5867a7cb7ccee25ff74051e368cd49c38d12c6d
Sean OMeara <sean@chef.io>
2016-02-22 18:09:33 -0500

    tweaking travis

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -2,36 +2,15 @@
 set -x
 set -e
 
-# Install docker
-# sudo mkdir -p /opt/docker
-# sudo curl -fo /opt/docker/docker "https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}"
-# sudo chmod +x /opt/docker/docker
-
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# enable backports
 echo 'deb "https://apt.dockerproject.org/repo" wily-backports main' >> /etc/apt/sources.list.d/wily-backports.list
+
+# enable docker repo
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb "https://apt.dockerproject.org/repo" ubuntu-wily main' >> /etc/apt/sources.list.d/docker-main.list
+
+# update apt cache
 apt-get -y update
+
+# install package
 apt-get -y install docker-engine=${DOCKER_VERSION}-0~wily
-# service docker start
-
-# case "${DOCKER_VERSION}" in
-#     "1.6.2" )
-#         DAEMON_ARG="-d"
-#         ;;
-#     *)
-#         DAEMON_ARG="daemon"
-#         ;;
-# esac
-
-# running=0
-# for x in {1..3}
-# do
-#     [[ $running != 1 ]] || break
-#     rm -rf /var/run/docker.pid
-#     /opt/docker/docker ${DAEMON_ARG} -D &
-#     DOCKER_PID=$!
-#     sleep 5
-#     echo "Checking if docker is running"
-#     ps -p $DOCKER_PID && running=1 || echo "Couldn't start docker, retrying"
-# done
-# echo "Docker running, continuing"
