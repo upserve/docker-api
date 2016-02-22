0999ea57085d8e0b6f2ae190197a8959aeaf0db9
Sean OMeara <sean@chef.io>
2016-02-22 18:07:43 -0500

    tweaking travis

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -8,6 +8,7 @@ set -e
 # sudo chmod +x /opt/docker/docker
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo 'deb "https://apt.dockerproject.org/repo" wily-backports main' >> /etc/apt/sources.list.d/wily-backports.list
 echo 'deb "https://apt.dockerproject.org/repo" ubuntu-wily main' >> /etc/apt/sources.list.d/docker-main.list
 apt-get -y update
 apt-get -y install docker-engine=${DOCKER_VERSION}-0~wily
