6749a6d67b292c3aad415d9806053287f1909bec
Sean OMeara <sean@chef.io>
2016-02-22 17:01:34 -0500

    tweaking travis

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -1,24 +1,14 @@
-# echo "Docker running, continuing"
-# done
-#     ps -p $DOCKER_PID && running=1 || echo "Couldn't start docker, retrying"
-#     echo "Checking if docker is running"
-#     sleep 5
-#     DOCKER_PID=$!
-#     /opt/docker/docker ${DAEMON_ARG} -D &
-#     rm -rf /var/run/docker.pid
-#     [[ $running != 1 ]] || break
-# do
-# for x in {1..3}
-# running=0
+#!/bin/bash
+set -x
+set -e
 
-# esac
-#         ;;
-#         DAEMON_ARG="daemon"
-#     *)
-#         ;;
-#         DAEMON_ARG="-d"
-#     "1.6.2" )
-# case "${DOCKER_VERSION}" in
+# Update bundler
+gem install bundler
+
+# Install docker
+# sudo mkdir -p /opt/docker
+# sudo curl -fo /opt/docker/docker "https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}"
+# sudo chmod +x /opt/docker/docker
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb "https://apt.dockerproject.org/repo" ubuntu-wily main' >> /etc/apt/sources.list.d/docker-main.list
@@ -26,14 +16,24 @@ apt-get -y update
 apt-get -y install docker-engine=${DOCKER_VERSION}-0~wily
 # service docker start
 
-# sudo chmod +x /opt/docker/docker
-# sudo curl -fo /opt/docker/docker "https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}"
-# sudo mkdir -p /opt/docker
-# Install docker
+# case "${DOCKER_VERSION}" in
+#     "1.6.2" )
+#         DAEMON_ARG="-d"
+#         ;;
+#     *)
+#         DAEMON_ARG="daemon"
+#         ;;
+# esac
 
-# gem install bundler
-# Update bundler
-
-set -e
-set -x
-#!/bin/bash
+# running=0
+# for x in {1..3}
+# do
+#     [[ $running != 1 ]] || break
+#     rm -rf /var/run/docker.pid
+#     /opt/docker/docker ${DAEMON_ARG} -D &
+#     DOCKER_PID=$!
+#     sleep 5
+#     echo "Checking if docker is running"
+#     ps -p $DOCKER_PID && running=1 || echo "Couldn't start docker, retrying"
+# done
+# echo "Docker running, continuing"
