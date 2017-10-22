124923c616227957baac79e8aa9fea52efebd0c6
Justus Flerlage <justusflerlage@juteel.com>
2017-10-23 01:56:12 +0200

    added swarm class and its inspect method

---
+++ lib/docker/swarm.rb
@@ -0,0 +1,11 @@
+# This class represents a Docker Swarm instance. It's important to note that nothing
+# is cached so that the information is always up to date.
+class Docker::Swarm
+  include Docker::Base
+
+  def self.inspect(conn = Docker.connection)
+    new(conn, Docker::Util.parse_json(conn.get('/swarm')))
+  end
+  
+  private_class_method :new
+end
