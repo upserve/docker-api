7c4cbd0016f7c34e7494547475596754f61fc6de
Justus Flerlage <justusflerlage@juteel.com>
2017-10-23 02:21:22 +0200

    added requires for swarm aand service class

--- lib/docker.rb
+++ lib/docker.rb
@@ -31,6 +31,8 @@ module Docker
   require 'docker/image'
   require 'docker/messages_stack'
   require 'docker/messages'
+  require 'docker/service'
+  require 'docker/swarm'
   require 'docker/util'
   require 'docker/version'
   require 'docker/volume'
