ad50232243d029af488fa54028fb99a950b03281
Todd Lunter <todd.lunter@lightspeedhq.com>
2024-07-08 13:10:49 -0400

    Update build config

--- script/install_podman.sh
+++ script/install_podman.sh
@@ -5,7 +5,7 @@ set -ex
 
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
 
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /" > /etc/apt/sources.list.d/podman.list
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" > /etc/apt/sources.list.d/podman.list
 
 apt-get update
 
