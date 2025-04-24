f52be4dc7c034949d5cb449e5156cad43ebd1dc9
Daniel Schmidt <dcschmidt@berkeley.edu>
2025-04-24 15:32:43 -0700

    Buildfix: pin rspec-its; simplify podman install (resolves #597)
    - Pins rspec-its to 1.x
    - Removes unnecessary setup steps for Ubuntu 20.10+ from ./scripts/install_podman.sh

--- script/install_podman.sh
+++ script/install_podman.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 set -ex
 
-. /etc/os-release
-
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
-
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" > /etc/apt/sources.list.d/podman.list
-
 apt-get update
-
 apt-get install -y podman
